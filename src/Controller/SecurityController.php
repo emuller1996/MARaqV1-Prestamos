8f31e0641c3ea6097c5e1e4e434c09d193f30ee3
emuller1996 <stefano.muller@hotmail.com>
2024-03-24 15:17:59 -0500

    FIX LOGIN NORMAL

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,6 +24,21 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    #[Route(path: '/login_check', name: 'app_login_check')]
+    public function loginCheck(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
