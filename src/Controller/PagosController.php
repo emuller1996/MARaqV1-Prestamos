8b5a2143568c8069fe64ad14f90f9781e20deb0a
emuller1996 <stefano.muller@hotmail.com>
2024-03-24 01:32:50 -0500

    NUEVO ESTILO

--- src/Controller/PagosController.php
+++ src/Controller/PagosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PagosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class PagosController extends AbstractController
 {
     #[Route('/pagos', name: 'app_pagos')]
-    public function index(): Response
+    public function index( PagosRepository $pagosRepository): Response
     {
+
+        $pagos = $pagosRepository->findAll();
         return $this->render('pagos/index.html.twig', [
-            'controller_name' => 'PagosController',
+            'pagos' => $pagos,
         ]);
     }
 }
